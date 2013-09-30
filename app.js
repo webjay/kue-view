4838093369ff334b79eeda287596e444ad13ef03
Jacob Friis Saxberg <jacob@saxberg.dk>
2013-09-30 12:17:41 -0400

    simpler

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ kue.redis.createClient = function(){
 };
 
 kue.app.set('title', 'Konfect Kue');
+kue.app.listen(process.env.PORT || 3000);
 
-
-var app = express();
-app.use(express.basicAuth('hello', 'joe'));
-app.use(kue.app);
-app.listen(process.env.PORT || 3000);
+// var app = express();
+// app.use(express.basicAuth('hello', 'joe'));
+// app.use(kue.app);
+// app.listen(process.env.PORT || 3000);
