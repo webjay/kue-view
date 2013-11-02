2601395de5fea7c61bb1ecdf588b19a59a912dc9
Jacob Friis Saxberg <jacob@saxberg.dk>
2013-11-02 01:54:42 -0400

    go public

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 var kue = require('kue');
-var redis = require('redis');
 var express = require('express');
+//var redis = require('redis');
 
-kue.redis.createClient = function(){
-  var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
-	if (process.env.REDIS_AUTH) {
-		client.auth(process.env.REDIS_AUTH);
-	}
-  client.on('error', function (err) {
-    console.error(err);
-  });
-  return client;
-};
+// kue.redis.createClient = function(){
+//   var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+//   if (process.env.REDIS_AUTH) {
+//     client.auth(process.env.REDIS_AUTH);
+//   }
+//   client.on('error', function (err) {
+//     console.error(err);
+//   });
+//   return client;
+// };
 
 kue.app.set('title', 'Konfect Kue');
 
 var app = express();
-app.use(express.basicAuth('hello', 'joe'));
+app.use(express.basicAuth(process.env.USERNAME, process.env.PASSWD));
 app.use(express.compress());
 app.use(kue.app);
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
