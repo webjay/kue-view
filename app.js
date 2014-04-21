870812f30cc805daf996193cee6f99481456a39d
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-04-21 01:19:16 -0400

    package versions+

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 var kue = require('kue');
 var express = require('express');
-//var redis = require('redis');
-
-// kue.redis.createClient = function(){
-//   var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
-//   if (process.env.REDIS_AUTH) {
-//     client.auth(process.env.REDIS_AUTH);
-//   }
-//   client.on('error', function (err) {
-//     console.error(err);
-//   });
-//   return client;
-// };
 
 kue.app.set('title', 'Konfect Kue');
 
